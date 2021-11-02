aa33eafb2f3ad282b20eb318c793a5fded93340f
Sahan Fernando <sahan.h.fernando@gmail.com>
2021-11-02 22:37:59 +1100

    Fix the env activation script

--- activate.sh
+++ activate.sh
@@ -2,6 +2,6 @@
 
 export SC2PATH="/media/huge/Stuff/Games/starcraft-ii/drive_c/Program Files (x86)/StarCraft II/"
 export SC2PF=WineLinux
-export WINEPREFIX=/media/huge/Stuff/Games/starcraft-ii/
+export WINEPREFIX=/media/huge/Stuff/Games/starcraft-ii/starcraft-ii/
 export WINE=~/.local/share/lutris/runners/wine/lutris-6.4-x86_64/bin/wine
 . .env/bin/activate
