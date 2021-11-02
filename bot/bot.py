1f0a919ac1cd7a3a92fcacfd3030da2fc89e3616
Sahan Fernando <sahan.h.fernando@gmail.com>
2021-11-02 20:24:22 +1100

    Made simple 12-pool 3 hatch zerg rush bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,10 +1,12 @@
-from sc2 import BotAI, Race
+from sc2 import BotAI, Race, UnitTypeId
+from random import choice
 
+WORKER_CAP = 16
 
 class CompetitiveBot(BotAI):
     NAME: str = "CompetitiveBot"
-    """This bot's name"""
-    RACE: Race = Race.Terran
+    """Fax"""
+    RACE: Race = Race.Zerg
     """This bot's Starcraft 2 race.
     Options are:
         Race.Terran
@@ -13,13 +15,48 @@ class CompetitiveBot(BotAI):
         Race.Random
     """
 
+    def count_building(self, building_type):
+        return self.already_pending(building_type) + self.structures.filter(lambda s: s.type_id == building_type and s.is_ready).amount
+
     async def on_start(self):
         print("Game started")
         # Do things here before the game starts
 
+    async def try_build(self, building_type):
+        if self.can_afford(building_type):
+            print(f"Morphing {building_type}")
+            pos = self.start_location.towards(self.game_info.map_center, distance=5)
+            placement = await self.find_placement(building_type, near=pos, placement_step=1)
+            if placement:
+                build_worker = self.workers.closest_to(placement)
+                build_worker.build(building_type, placement)
+
     async def on_step(self, iteration):
-        # Populate this function with whatever your bot should do!
-        pass
+        # Create a spawning pool if one doesn't exist
+        my_larva = self.larva
+        if self.count_building(UnitTypeId.SPAWNINGPOOL) == 0:
+            await self.try_build(UnitTypeId.SPAWNINGPOOL)
+            return
+        if self.count_building(UnitTypeId.HATCHERY) < 3 and self.workers.amount == WORKER_CAP:
+            await self.try_build(UnitTypeId.HATCHERY)
+        if len(my_larva) > 0:
+            if self.supply_left > 0 and self.workers.amount < WORKER_CAP and self.can_afford(UnitTypeId.DRONE):
+                my_larva.random.train(UnitTypeId.DRONE)
+            elif self.supply_left > 0 and self.can_afford(UnitTypeId.ZERGLING):
+                # Create lings if we can
+                print("Morphing ling")
+                my_larva.random.train(UnitTypeId.ZERGLING)
+            elif self.can_afford(UnitTypeId.OVERLORD):
+                # Create overlord if no supply left
+                print("Morphing overlord")
+                my_larva.random.train(UnitTypeId.OVERLORD)
+        # Order idle lings to attack a random enemy position if more than 14 lings
+        num_lings = self.units.filter(lambda u: u.is_ready and u.type_id == UnitTypeId.ZERGLING).amount
+        if num_lings >= 18:
+            idle_lings = self.units.filter(lambda u: u.is_idle and u.is_ready and u.type_id == UnitTypeId.ZERGLING)
+            for ling in idle_lings:
+                ling.attack(choice(self.enemy_start_locations))
+        
 
     def on_end(self, result):
         print("Game ended.")
