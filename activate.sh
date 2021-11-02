a783dc44ecfc86435bf2240a7b4f62decdfbd127
Sahan Fernando <sahan.h.fernando@gmail.com>
2021-11-02 20:23:47 +1100

    Added activation script

---
+++ activate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export SC2PATH="/media/huge/Stuff/Games/starcraft-ii/drive_c/Program Files (x86)/StarCraft II/"
+export SC2PF=WineLinux
+export WINEPREFIX=/media/huge/Stuff/Games/starcraft-ii/
+export WINE=~/.local/share/lutris/runners/wine/lutris-6.4-x86_64/bin/wine
+. .env/bin/activate
