0d21342ddfb46745f99f2a6d6528929b5b8a5877
Sahan Fernando <sahan.h.fernando@gmail.com>
2022-02-06 22:10:14 +1100

    Lower attack detection threshold

--- src/strategy.rs
+++ src/strategy.rs
@@ -7,7 +7,7 @@ impl FaxBot {
         let structures = &self.units.my.structures;
         let attacking_units = self.units.enemy.units
             .filter(|u| !u.is_worker() && structures.closest_distance(u.position()).unwrap_or(9999.0) < 18.0);
-        let is_under_attack = attacking_units.len() > 4;
+        let is_under_attack = attacking_units.len() >= 2;
         if is_under_attack != self.state.is_under_attack {
             println!("Under attack? {}", is_under_attack);
         }
