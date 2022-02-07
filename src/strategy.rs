aeee5903daa219016d1c01becbe593ce2f65ea70
Sahan Fernando <sahan.h.fernando@gmail.com>
2022-02-07 20:28:32 +1100

    Don't treat flying units as threats

--- src/strategy.rs
+++ src/strategy.rs
@@ -6,7 +6,7 @@ impl FaxBot {
     pub fn determine_state_for_tick(&mut self, _iteration: usize) {
         let structures = &self.units.my.structures;
         let attacking_units = self.units.enemy.units
-            .filter(|u| !u.is_worker() && structures.closest_distance(u.position()).unwrap_or(9999.0) < 18.0);
+            .filter(|u| !u.is_worker() && !u.is_flying() && structures.closest_distance(u.position()).unwrap_or(9999.0) < 18.0);
         let is_under_attack = attacking_units.len() >= 2;
         if is_under_attack != self.state.is_under_attack {
             println!("Under attack? {}", is_under_attack);
