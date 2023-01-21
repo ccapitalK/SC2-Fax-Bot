e0df9506eeefe50d73cf6e6677cf952ec72ccd42
Sahan Fernando <sahan.h.fernando@gmail.com>
2023-01-21 22:03:24 +1100

    Fix activate.sh

--- activate.sh
+++ activate.sh
@@ -2,7 +2,7 @@
 
 export SC2PATH="/media/huge/Stuff/Games/starcraft-ii/drive_c/Program Files (x86)/StarCraft II/"
 export WINEESYNC=1
-export PATH="$HOME/.local/share/lutris/runners/wine/lutris-fshack-6.14-3-x86_64/bin/:$PATH"
+export PATH="$HOME/.local/share/lutris/runners/wine/lutris-fshack-6.21-6-x86_64/bin/:$PATH"
 export WINEPREFIX=/media/huge/Stuff/Games/starcraft-ii/
 export WINEDEBUG=-all
-export WINE=~/.local/share/lutris/runners/wine/lutris-fshack-6.14-3-x86_64/bin/wine
+export WINE=~/.local/share/lutris/runners/wine/lutris-fshack-6.21-6-x86_64/bin/wine
